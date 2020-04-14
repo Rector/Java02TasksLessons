752af2d8ba3927d6d7fc2d3b55c1e5f3211df8ba
Kirill <kirill261989@mail.ru>
2020-04-14 17:28:57 +0400

    add taskLesson04

---
+++ src/taskLesson04/ChatServer.java
@@ -0,0 +1,11 @@
+package taskLesson04;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
