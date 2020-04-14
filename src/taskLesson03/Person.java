505e56a89aad2610c581ba6617aef01aa9867741
Kirill <kirill261989@mail.ru>
2020-04-14 14:51:39 +0400

    add taskLesson03

---
+++ src/taskLesson03/Person.java
@@ -0,0 +1,11 @@
+package taskLesson03;
+
+public class Person {
+    public String phone;
+    public String eMail;
+
+    public Person(String phone, String eMail) {
+        this.phone = phone;
+        this.eMail = eMail;
+    }
+}
